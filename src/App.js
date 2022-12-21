6a21e5b03e5a220643f1d30038a30207297293c4
DedSeK404 <marwenbennaceur5@gmail.com>
2022-12-21 14:29:00 +0100

    chR

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Navbar, Container, Nav, NavDropdown, Row, Col, Image, Button, Card, CardGroup } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </header>
+      <main>
+        <Container>
+          <Row className="px-4 my-5">
+            <Col sm={7}>
+              <Image src="https://fabskill.s3.eu-west-3.amazonaws.com/bus_cover/63.jpg" fluid rounded className="" />
+            </Col>
+            <Col sm={5}>
+
+              <h1 class="font-weigh-light">Checkpoint</h1>
+              <p class="mt-4">
+                This is me doing the ReactJs Checkpoint, i'm currently testing if this paragraph will be under the h1 clean and organized.
+              </p>
+              <Button variant="outline-primary">It's Working!</Button>
+            </Col>
+          </Row>
+          <Row>
+            <Card className="text-center bg-secondary text-white my-5 py-4">
+              <Card.Body>This call to action card is a great place to showcase some important information or display a clever tagline</Card.Body>
+            </Card>
+          </Row>
+          <Row>
+            <Col>
+    <CardGroup>
+      <Card>
+        <Card.Img variant="top" src="https://picsum.photos/id/161/320/200" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://picsum.photos/id/219/320/200" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This card has supporting text below as a natural lead-in to
+            additional content.{' '}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+      <Card>
+        <Card.Img variant="top" src="https://picsum.photos/id/220/320/200" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+    </CardGroup>
+            </Col>
+          </Row>
+        </Container>
+      </main>
+      <footer class="py-5 my-5 bg-dark">
+<Container className="px-4">
+<p class="text-center text-white"> Copyright DedSeK, All rights reserved 2023 </p>
+</Container>
+      </footer>
     </div>
   );
 }
